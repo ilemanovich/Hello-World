ebf8f9a5d141290aea15573014a43b2f299daa6e
Ivan Lemanovich <ilemanovich@yandex.ru>
2022-07-28 23:54:36 +0300

    Mega init

---
+++ Hello World.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    
+    cout << "Enter an integer" << endl;
+    cin >> a;
+
+    cout << "There is your integer " << a << endl;
+
+    return 0;
+}
